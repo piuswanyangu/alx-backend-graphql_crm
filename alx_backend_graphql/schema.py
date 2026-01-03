b3073014b16e718bf41cc606fd35b8852e317f50
pius wanyangu <ndubipius96@gmail.com>
2026-01-03 23:29:55 +0300

    setting up graphql

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,6 @@
 import graphene
 
-class Query(graphene.ObjectType):
+class Query(CRMQuery, graphene.ObjectType):
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
